1b49c4d1dc2c22dfa82d60fc264ffa85936825c6
sudhir <sudhirhota200@gmail.com>
2023-07-13 13:29:46 -0700

    some changes in project

--- config/SecurityConfig.java
+++ config/SecurityConfig.java
@@ -1,8 +1,11 @@
 package com.blogapi.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.WebSecurityConfigurer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -20,36 +23,45 @@ import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    @Autowired
+    private CustomUserDetailsService userDetailsService;
     @Bean
     PasswordEncoder passwordEncoder(){
-
         return new BCryptPasswordEncoder();
     }
-
+    @Override
+    @Bean
+    public AuthenticationManager authenticationManagerBean() throws Exception
+    {
+        return super.authenticationManagerBean();
+    }
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-
         http
                 .csrf().disable()
                 .authorizeRequests()
                 .antMatchers(HttpMethod.GET, "/api/**").permitAll()
                 .antMatchers("/api/auth/**").permitAll()
-
                 .anyRequest()
                 .authenticated()
                 .and()
                 .httpBasic();
-
     }
     @Override
-    @Bean
-    protected UserDetailsService userDetailsService() {
-        UserDetails user =
-                User.builder().username("sudhir").password(passwordEncoder()
-                        .encode("8658")).roles("USER").build();
-        UserDetails admin =
-                User.builder().username("admin").password(passwordEncoder()
-                        .encode("admin")).roles("ADMIN").build();
-        return new InMemoryUserDetailsManager(user, admin);
+    protected void configure(AuthenticationManagerBuilder auth) throws
+            Exception {
+        auth.userDetailsService(userDetailsService)
+                .passwordEncoder(passwordEncoder());
     }
+//    @Override
+//    @Bean
+//    protected UserDetailsService userDetailsService() {
+//        UserDetails user =
+//                User.builder().username("sudhir").password(passwordEncoder()
+//                        .encode("8658")).roles("USER").build();
+//        UserDetails admin =
+//                User.builder().username("admin").password(passwordEncoder()
+//                        .encode("admin")).roles("ADMIN").build();
+//        return new InMemoryUserDetailsManager(user, admin);
+//    }
 }
--- controller/AuthController.java
+++ controller/AuthController.java
@@ -2,6 +2,7 @@ package com.blogapi.controller;
 
 import com.blogapi.entity.Role;
 import com.blogapi.entity.User;
+import com.blogapi.paylod.LoginDto;
 import com.blogapi.paylod.SignUpDto;
 import com.blogapi.repository.RoleRepository;
 import com.blogapi.repository.UserRepository;
@@ -9,6 +10,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -26,6 +31,9 @@ public class AuthController {
     PasswordEncoder passwordEncoder;
  @Autowired
  private RoleRepository roleRepository;
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
 
 
 //localhost:8080/api/auth/signup
@@ -54,6 +62,21 @@ public class AuthController {
         return new ResponseEntity<>("User registered successfully",
                 HttpStatus.OK);
     }
+   // localhost:8080/api/auth/signin
+    @PostMapping("/signin")
+    public ResponseEntity<String> authenticateUser(@RequestBody LoginDto loginDto) {
+
+
+        Authentication authentication = authenticationManager.authenticate(
+
+                new UsernamePasswordAuthenticationToken(loginDto.getUsernameOrEmail(),loginDto.getPassword()) );
+
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+
+        return new ResponseEntity<>("User signed-in successfully!.", HttpStatus.OK);
+
+    }
+
 }
 
 
